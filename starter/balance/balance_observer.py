933ad72b4d625faf180448c8b46eb2efd18e258f
johngrindall <john@2simple.com>
2026-08-12 17:55:00 +0100

    add the logic for observing

--- starter/balance/balance_observer.py
+++ starter/balance/balance_observer.py
@@ -10,11 +10,18 @@ class PrintObserver(IBalanceObserver):
 
 
 class LowBalanceAlertObserver(IBalanceObserver):
+
+    alert_triggered: bool = False
+    threshold: int
+
     def __init__(self, threshold):
         self.threshold = threshold
+        self.alert_triggered = False
 
     def update(self, balance, transaction):
         """Alert if balance drops below threshold."""
         balance = balance.get_balance()
         if balance < self.threshold:
             print(f"Alert! Low balance: {balance} < {self.threshold}")
+            self.alert_triggered = True
+
